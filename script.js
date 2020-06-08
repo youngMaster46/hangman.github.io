f3c9bbf4cba9cee915e0644ff41cbbd14be7157e
Maxim Kaplin <kaplin95@inbox.ru>
2020-06-08 14:16:51 +0300

    add notification and event on keys

--- script.js
+++ script.js
@@ -30,4 +30,45 @@ function displayWord() {
         popup.style.display = 'flex';
     }
 }
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+    console.log('Update wrong');
+}
+
+// Show notification 
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show')
+    }, 2000)
+}
+
+window.addEventListener('keydown', e => {
+    // console.log(e.keyCode);
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+
+                displayWord()
+            } else {
+                showNotification()
+            }
+        } else {
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter)
+
+                updateWrongLettersEl();
+            }
+            else {
+                showNotification();
+            }
+        }
+    }
+})
+
 displayWord()
